601fe09c4e96ed083a11748f1fa54307a6c71a80
d.kruhlov <dkruglov@dataart.com>
2026-07-07 12:39:05 +0200

    main: drop FE task

--- sticky-notes/src/domain/constants.ts
+++
@@ -1,5 +0,0 @@
-import type { Size } from './types';
-
-export const MIN_NOTE_SIZE: Size = { width: 120, height: 90 };
-export const DEFAULT_NOTE_SIZE: Size = { width: 180, height: 150 };
-export const MIN_DRAW_SIZE = 24;
